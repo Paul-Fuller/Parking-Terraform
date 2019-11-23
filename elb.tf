d73af55016f3a62ec09b57eae937cae3a6a0d1b9
Paul.Fuller <mr_pfuller@hotmail.com>
2019-11-23 18:06:41 +0000

    Add s3 and elb files

---
+++ elb.tf
@@ -0,0 +1,46 @@
+#########################################
+# Elastic Load Balancer
+#########################################
+
+resource "aws_elb" "elb" {
+  name               = "parking-elb"
+  availability_zones = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+
+  access_logs {
+    bucket        = "parking-s3" # (Required) The S3 bucket name to store the logs in. 
+    interval      = 60
+  }
+
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  # listener {
+  #   instance_port      = 8000
+  #   instance_protocol  = "http"
+  #   lb_port            = 443
+  #   lb_protocol        = "https"
+  #   ssl_certificate_id = "arn:aws:iam::123456789012:server-certificate/certName"
+  # }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+
+  instances                   = ["${aws_instance.foo.id}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "foobar-terraform-elb"
+  }
+}
